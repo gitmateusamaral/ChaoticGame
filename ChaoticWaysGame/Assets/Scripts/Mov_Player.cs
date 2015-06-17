35d3015c6b671ad80c9ed95b187098691f9398d0
Mateus Amaral <reiclick123@hotmail.com>
2015-06-17 19:08:59 -0300

    GameWip
    ChaoticWays

---
+++ ChaoticWaysGame/Assets/Scripts/Mov_Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mov_Player : MonoBehaviour 
+{
+	string mov = "stop";
+	float finPos;
+
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1") && mov == "stop") 
+		{
+			mov = "left";
+			finPos =  transform.position.x - 10;
+		}
+		if (Input.GetButtonDown ("Fire2") && mov == "stop") 
+		{
+			mov = "right";
+			finPos =  transform.position.x + 10;
+		}
+		transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+25*Time.deltaTime);
+		
+		if(mov == "right")
+		{
+			transform.position = new Vector3(transform.position.x+25*Time.deltaTime,transform.position.y,transform.position.z);
+			if(transform.position.x >= finPos)
+			{
+				mov = "stop";
+			}
+		}
+		if(mov == "left")
+		{
+			transform.position = new Vector3(transform.position.x-25*Time.deltaTime,transform.position.y,transform.position.z);
+			if(transform.position.x <= finPos)
+			{
+				mov = "stop";
+			}
+		}
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "la")
+		{
+			mov = "stop";
+			transform.position = new Vector3(12,2,4);
+		}
+	}
+	
+}
